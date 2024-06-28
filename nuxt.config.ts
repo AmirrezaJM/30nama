2d7b57d163549af50910d80d72ee2fd5898dcd2c
AmirrezaJM <amirreza.jolani@yahoo.com>
2024-06-28 22:00:18 +0200

    30nama application with nuxt 3

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+
+const apiBaseURL = 'https://movies-proxy.vercel.app'
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  
+})
